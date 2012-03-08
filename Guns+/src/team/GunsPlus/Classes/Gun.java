4984116d09381371d8480401644d7c1f6b646ba5
Robert Klassert <robert.klassert@arcor.de>
2012-03-08 19:21:27 +0100

    added zoom, reload, fire methods

--- Guns+/src/team/GunsPlus/Classes/Gun.java
+++ Guns+/src/team/GunsPlus/Classes/Gun.java
@@ -2,11 +2,24 @@ package team.GunsPlus.Classes;
 
 import org.bukkit.plugin.Plugin;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
+import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class Gun extends GenericCustomItem{
 
 	public Gun(Plugin plugin, String name, String texture) {
 		super(plugin, name, texture);
 	}
-	//TODO: Gun Coding
+
+	public void zoom(SpoutPlayer sp){
+		
+	}
+	
+	public void fire(SpoutPlayer sp){
+		
+	}
+	
+	public void reload(SpoutPlayer sp){
+		
+	}
+	
 }
