92c100d2e0b5a0726573be8c82ac1b465ef9f121
Robert Klassert <robert.klassert@arcor.de>
2012-03-08 21:23:32 +0100

    added Ammo field

--- Guns+/src/team/GunsPlus/Classes/Gun.java
+++ Guns+/src/team/GunsPlus/Classes/Gun.java
@@ -1,11 +1,18 @@
 package team.GunsPlus.Classes;
 
+import java.util.ArrayList;
+
+
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class Gun extends GenericCustomItem{
 
+	//PRIVATE PROPERTIES
+	private ArrayList<ItemStack> ammo = new ArrayList<ItemStack>();
+	
 	public Gun(Plugin plugin, String name, String texture) {
 		super(plugin, name, texture);
 	}
@@ -20,6 +27,17 @@ public class Gun extends GenericCustomItem{
 	
 	public void reload(SpoutPlayer sp){
 		
+	}
+
+	
+	
+	//GETTERS AND SETTERS
+	public ArrayList<ItemStack> getAmmo() {
+		return ammo;
+	}
+
+	public void setAmmo(ArrayList<ItemStack> ammo) {
+		this.ammo = ammo;
 	}
 	
 }
