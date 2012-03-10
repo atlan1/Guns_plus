1a3de34dc5f5ad2056c894c15ebcf9ac277696ba
Robert Klassert <robert.klassert@arcor.de>
2012-03-10 21:14:24 +0100

    added effect list

--- Guns+/src/team/GunsPlus/Classes/Gun.java
+++ Guns+/src/team/GunsPlus/Classes/Gun.java
@@ -10,12 +10,15 @@ import org.bukkit.plugin.Plugin;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
+import team.GunsPlus.EffectType;
+
 public class Gun extends GenericCustomItem{
 
 	//PRIVATE PROPERTIES
-	private ArrayList<ItemStack> ammo = new ArrayList<ItemStack>();
+	private ArrayList<ItemStack> ammo = new ArrayList<ItemStack>(); //Holds all accepted ammo itemstacks
 	private Map<String,Float> values = new HashMap<String,Float>(); //Holds Damage, Recoil, Etc in single location
 	private Map<String, String> resources = new HashMap<String, String>(); //Holds file resources like texture and sounds
+	private ArrayList<EffectType> effects = new ArrayList<EffectType>(); //Holds the effects of the gun
 	
 	public Gun(Plugin plugin, String name, String texture) {
 		super(plugin, name, texture);
@@ -33,8 +36,6 @@ public class Gun extends GenericCustomItem{
 	public void reload(SpoutPlayer sp){
 		
 	}
-
-	
 	
 	//GETTERS AND SETTERS
 	public ArrayList<ItemStack> getAmmo() {
@@ -61,6 +62,15 @@ public class Gun extends GenericCustomItem{
 		Float f = -1F;
 		if(values.containsKey(name)) f = values.get(name);
 		return f;
+	}
+	
+	public void addEffect(EffectType et){
+		effects.add(et);
+	}
+	
+	public void removeEffect(EffectType et){
+		if(effects.contains(et))
+			effects.remove(et);
 	}
 
 }
