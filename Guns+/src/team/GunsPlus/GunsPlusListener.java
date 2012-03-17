392c880960b473a518c0749784c72612553e5b67
SirTyler <sirtyler132@gmail.com>
2012-03-17 13:39:18 -0400

    Signed-off-by: SirTyler <sirtyler132@gmail.com>

--- Guns+/src/team/GunsPlus/GunsPlusListener.java
+++ Guns+/src/team/GunsPlus/GunsPlusListener.java
@@ -2,7 +2,6 @@ package team.GunsPlus;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,127 +20,178 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 import team.GunsPlus.Enum.KeyType;
 import team.GunsPlus.Item.Gun;
 
-public class GunsPlusListener implements Listener{
+public class GunsPlusListener implements Listener {
 
 	public GunsPlus plugin;
-	
-	public GunsPlusListener(GunsPlus instance){
+
+	public GunsPlusListener(GunsPlus instance) {
 		plugin = instance;
 	}
-	
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onPlayerInteract(PlayerInteractEvent e){
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerInteract(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
-		if(!Util.hasSpoutcraft(p)) return;
+		if (GunsPlus.lwc != null)
+			GunsPlus.lwc.wrapPlayer(p);
+		if (!Util.hasSpoutcraft(p))
+			return;
 		SpoutPlayer sp = (SpoutPlayer) p;
-		if(!GunUtils.holdsGun(sp)) return;
+		if (!GunUtils.holdsGun(sp))
+			return;
 		Action a = e.getAction();
-		Block b = e.getClickedBlock();
-		if(Util.isBlockAction(a))
-			if(!GunsPlus.lwc.canAccessProtection(sp, b.getX(), b.getY(), b.getZ())){
-				e.setCancelled(true);
-				return;
-			}
 		Gun g = GunUtils.getGunInHand(sp);
-		switch(a){
-			case RIGHT_CLICK_AIR:
-				if(plugin.zoomKey.equals(KeyType.RIGHT)) g.zoom(sp);
-				if(plugin.reloadKey.equals(KeyType.RIGHT)) g.reload(sp);
-				if(plugin.fireKey.equals(KeyType.RIGHT)) g.fire(sp);
-				if(sp.isSneaking()&&plugin.zoomKey.equals(KeyType.RIGHTSHIFT)) g.zoom(sp);
-				if(sp.isSneaking()&&plugin.reloadKey.equals(KeyType.RIGHTSHIFT)) g.reload(sp);
-				if(sp.isSneaking()&&plugin.fireKey.equals(KeyType.RIGHTSHIFT)) g.fire(sp);
-				break;
-			case RIGHT_CLICK_BLOCK:
-				if(plugin.zoomKey.equals(KeyType.RIGHT)) g.zoom(sp);
-				if(plugin.reloadKey.equals(KeyType.RIGHT)) g.reload(sp);
-				if(plugin.fireKey.equals(KeyType.RIGHT)) g.fire(sp);
-				if(sp.isSneaking()&&plugin.zoomKey.equals(KeyType.RIGHTSHIFT)) g.zoom(sp);
-				if(sp.isSneaking()&&plugin.reloadKey.equals(KeyType.RIGHTSHIFT)) g.reload(sp);
-				if(sp.isSneaking()&&plugin.fireKey.equals(KeyType.RIGHTSHIFT)) g.fire(sp);
-				break;
-			case LEFT_CLICK_AIR:
-				if(plugin.zoomKey.equals(KeyType.LEFT)) g.zoom(sp);
-				if(plugin.reloadKey.equals(KeyType.LEFT)) g.reload(sp);
-				if(plugin.fireKey.equals(KeyType.LEFT)) g.fire(sp);
-				if(sp.isSneaking()&&plugin.zoomKey.equals(KeyType.LEFTSHIFT)) g.zoom(sp);
-				if(sp.isSneaking()&&plugin.reloadKey.equals(KeyType.LEFTSHIFT)) g.reload(sp);
-				if(sp.isSneaking()&&plugin.fireKey.equals(KeyType.LEFTSHIFT)) g.fire(sp);
-				break;
-			case LEFT_CLICK_BLOCK:
-				if(plugin.zoomKey.equals(KeyType.LEFT)) g.zoom(sp);
-				if(plugin.reloadKey.equals(KeyType.LEFT)) g.reload(sp);
-				if(plugin.fireKey.equals(KeyType.LEFT)) g.fire(sp);
-				if(sp.isSneaking()&&plugin.zoomKey.equals(KeyType.LEFTSHIFT)) g.zoom(sp);
-				if(sp.isSneaking()&&plugin.reloadKey.equals(KeyType.LEFTSHIFT)) g.reload(sp);
-				if(sp.isSneaking()&&plugin.fireKey.equals(KeyType.LEFTSHIFT)) g.fire(sp);
-				break;
+		switch (a) {
+		case RIGHT_CLICK_AIR:
+			if (plugin.zoomKey.equals(KeyType.RIGHT))
+				g.zoom(sp);
+			if (plugin.reloadKey.equals(KeyType.RIGHT))
+				g.reload(sp);
+			if (plugin.fireKey.equals(KeyType.RIGHT))
+				g.fire(sp);
+			if (sp.isSneaking() && plugin.zoomKey.equals(KeyType.RIGHTSHIFT))
+				g.zoom(sp);
+			if (sp.isSneaking() && plugin.reloadKey.equals(KeyType.RIGHTSHIFT))
+				g.reload(sp);
+			if (sp.isSneaking() && plugin.fireKey.equals(KeyType.RIGHTSHIFT))
+				g.fire(sp);
+			break;
+		case RIGHT_CLICK_BLOCK:
+			if (plugin.zoomKey.equals(KeyType.RIGHT))
+				g.zoom(sp);
+			if (plugin.reloadKey.equals(KeyType.RIGHT))
+				g.reload(sp);
+			if (plugin.fireKey.equals(KeyType.RIGHT))
+				g.fire(sp);
+			if (sp.isSneaking() && plugin.zoomKey.equals(KeyType.RIGHTSHIFT))
+				g.zoom(sp);
+			if (sp.isSneaking() && plugin.reloadKey.equals(KeyType.RIGHTSHIFT))
+				g.reload(sp);
+			if (sp.isSneaking() && plugin.fireKey.equals(KeyType.RIGHTSHIFT))
+				g.fire(sp);
+			break;
+		case LEFT_CLICK_AIR:
+			if (plugin.zoomKey.equals(KeyType.LEFT))
+				g.zoom(sp);
+			if (plugin.reloadKey.equals(KeyType.LEFT))
+				g.reload(sp);
+			if (plugin.fireKey.equals(KeyType.LEFT))
+				g.fire(sp);
+			if (sp.isSneaking() && plugin.zoomKey.equals(KeyType.LEFTSHIFT))
+				g.zoom(sp);
+			if (sp.isSneaking() && plugin.reloadKey.equals(KeyType.LEFTSHIFT))
+				g.reload(sp);
+			if (sp.isSneaking() && plugin.fireKey.equals(KeyType.LEFTSHIFT))
+				g.fire(sp);
+			break;
+		case LEFT_CLICK_BLOCK:
+			if (plugin.zoomKey.equals(KeyType.LEFT))
+				g.zoom(sp);
+			if (plugin.reloadKey.equals(KeyType.LEFT))
+				g.reload(sp);
+			if (plugin.fireKey.equals(KeyType.LEFT))
+				g.fire(sp);
+			if (sp.isSneaking() && plugin.zoomKey.equals(KeyType.LEFTSHIFT))
+				g.zoom(sp);
+			if (sp.isSneaking() && plugin.reloadKey.equals(KeyType.LEFTSHIFT))
+				g.reload(sp);
+			if (sp.isSneaking() && plugin.fireKey.equals(KeyType.LEFTSHIFT))
+				g.fire(sp);
+			break;
 		}
 	}
-	
-	//does not work pretty much? dont know why...
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onHeldItemChange(PlayerItemHeldEvent e){
+
+	// does not work pretty much? dont know why...
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onHeldItemChange(PlayerItemHeldEvent e) {
 		Player p = e.getPlayer();
-		if(!Util.hasSpoutcraft(p)) return;
+		if (!Util.hasSpoutcraft(p))
+			return;
 		SpoutPlayer sp = (SpoutPlayer) p;
- 		ItemStack preItem = p.getInventory().getItem(e.getPreviousSlot());
+		ItemStack preItem = p.getInventory().getItem(e.getPreviousSlot());
 		ItemStack nextItem = p.getInventory().getItem(e.getNewSlot());
-		if(GunUtils.isGun(preItem)){
+		if (GunUtils.isGun(preItem)) {
 			sp.setWalkingMultiplier(1);
 		}
-		if(GunUtils.isGun(nextItem)){
+		if (GunUtils.isGun(nextItem)) {
 			Gun g = GunUtils.getGun(nextItem);
-			sp.setWalkingMultiplier(1-g.getValue("WEIGHT"));
-			
+			sp.setWalkingMultiplier(1 - g.getValue("WEIGHT"));
+
 		}
 	}
-	
-	
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onKeyPressed(KeyPressedEvent e){
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onKeyPressed(KeyPressedEvent e) {
 		SpoutPlayer sp = e.getPlayer();
-		if(!GunUtils.holdsGun(sp)) return;
-		
+		if (!GunUtils.holdsGun(sp))
+			return;
+
 		Gun g = GunUtils.getGunInHand(sp);
 		Keyboard key = e.getKey();
 		String keyString = key.toString().split("_")[1].toLowerCase();
 		ScreenType st = e.getScreenType();
-		
-		if((plugin.zoomKey.equals(KeyType.LETTER)||plugin.zoomKey.equals(KeyType.NUMBER)||plugin.zoomKey.equals(KeyType.HOLDNUMBER)||plugin.zoomKey.equals(KeyType.HOLDLETTER))&&plugin.zoomKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
+
+		if ((plugin.zoomKey.equals(KeyType.LETTER)
+				|| plugin.zoomKey.equals(KeyType.NUMBER)
+				|| plugin.zoomKey.equals(KeyType.HOLDNUMBER) || plugin.zoomKey
+					.equals(KeyType.HOLDLETTER))
+				&& plugin.zoomKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
 			g.zoom(sp);
-		}else if((plugin.reloadKey.equals(KeyType.LETTER)||plugin.reloadKey.equals(KeyType.NUMBER)||plugin.reloadKey.equals(KeyType.HOLDNUMBER)||plugin.reloadKey.equals(KeyType.HOLDLETTER))&&plugin.reloadKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
+		} else if ((plugin.reloadKey.equals(KeyType.LETTER)
+				|| plugin.reloadKey.equals(KeyType.NUMBER)
+				|| plugin.reloadKey.equals(KeyType.HOLDNUMBER) || plugin.reloadKey
+					.equals(KeyType.HOLDLETTER))
+				&& plugin.reloadKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
 			g.reload(sp);
-		}else if((plugin.fireKey.equals(KeyType.LETTER)||plugin.fireKey.equals(KeyType.NUMBER)||plugin.fireKey.equals(KeyType.HOLDNUMBER)||plugin.fireKey.equals(KeyType.HOLDLETTER))&&plugin.fireKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
+		} else if ((plugin.fireKey.equals(KeyType.LETTER)
+				|| plugin.fireKey.equals(KeyType.NUMBER)
+				|| plugin.fireKey.equals(KeyType.HOLDNUMBER) || plugin.fireKey
+					.equals(KeyType.HOLDLETTER))
+				&& plugin.fireKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
 			g.fire(sp);
 		}
 	}
-	
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onKeyReleased(KeyReleasedEvent e){
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onKeyReleased(KeyReleasedEvent e) {
 		SpoutPlayer sp = e.getPlayer();
-		if(!GunUtils.holdsGun(sp)) return;
-		
+		if (!GunUtils.holdsGun(sp))
+			return;
+
 		Gun g = GunUtils.getGunInHand(sp);
 		Keyboard key = e.getKey();
 		String keyString = key.toString().split("_")[1].toLowerCase();
 		ScreenType st = e.getScreenType();
-		
-		if((plugin.zoomKey.equals(KeyType.HOLDNUMBER)||plugin.zoomKey.equals(KeyType.HOLDLETTER))&&plugin.zoomKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
+
+		if ((plugin.zoomKey.equals(KeyType.HOLDNUMBER) || plugin.zoomKey
+				.equals(KeyType.HOLDLETTER))
+				&& plugin.zoomKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
 			g.zoom(sp);
-		}else if((plugin.reloadKey.equals(KeyType.HOLDNUMBER)||plugin.reloadKey.equals(KeyType.HOLDLETTER))&&plugin.reloadKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
-			//stop reloading ?
-		}else if((plugin.fireKey.equals(KeyType.HOLDNUMBER)||plugin.fireKey.equals(KeyType.HOLDLETTER))&&plugin.fireKey.getData().equalsIgnoreCase(keyString)&&st.toString().equalsIgnoreCase("GAME_SCREEN")){
-			//stop fireing ?
+		} else if ((plugin.reloadKey.equals(KeyType.HOLDNUMBER) || plugin.reloadKey
+				.equals(KeyType.HOLDLETTER))
+				&& plugin.reloadKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
+			// stop reloading ?
+		} else if ((plugin.fireKey.equals(KeyType.HOLDNUMBER) || plugin.fireKey
+				.equals(KeyType.HOLDLETTER))
+				&& plugin.fireKey.getData().equalsIgnoreCase(keyString)
+				&& st.toString().equalsIgnoreCase("GAME_SCREEN")) {
+			// stop fireing ?
 		}
 	}
-	
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onSpoutCraftEnable(SpoutCraftEnableEvent e){
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onSpoutCraftEnable(SpoutCraftEnableEvent e) {
 		SpoutPlayer sp = e.getPlayer();
-		HUD hud = new HUD(plugin, plugin.hudX, plugin.hudY, plugin.hudBackground);
+		HUD hud = new HUD(plugin, plugin.hudX, plugin.hudY,
+				plugin.hudBackground);
 		hud.start(sp);
 		GunsPlus.fireCounter.put(sp, 0);
-		sp.sendNotification(ChatColor.GRAY + "Guns+", ChatColor.DARK_GREEN + "By " + plugin.getDescription().getAuthors(), Material.SULPHUR);
+		sp.sendNotification(ChatColor.GRAY + "Guns+", ChatColor.DARK_GREEN
+				+ "By " + plugin.getDescription().getAuthors(),
+				Material.SULPHUR);
 	}
 }
--- Guns+/src/team/GunsPlus/Item/Gun.java
+++ Guns+/src/team/GunsPlus/Item/Gun.java
@@ -2,18 +2,25 @@ package team.GunsPlus.Item;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.block.SpoutBlock;
 import org.getspout.spoutapi.gui.GenericTexture;
 import org.getspout.spoutapi.gui.RenderPriority;
 import org.getspout.spoutapi.gui.WidgetAnchor;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
 import org.getspout.spoutapi.player.SpoutPlayer;
+
+import com.griefcraft.util.ProtectionFinder;
+import com.griefcraft.util.matchers.DoorMatcher;
+import com.griefcraft.util.matchers.DoubleChestMatcher;
 
 import team.GunsPlus.Enum.EffectType;
 import team.GunsPlus.Enum.Projectile;
@@ -37,6 +44,41 @@ public class Gun extends GenericCustomItem{
 		this.plugin = (GunsPlus) plugin;
 	}
 	
+	public boolean onItemInteract(SpoutPlayer player, SpoutBlock block,
+			org.bukkit.block.BlockFace face) {
+		try {
+			if (GunsPlus.lwc != null) {
+				GunsPlus.lwc.wrapPlayer(player.getPlayer());
+				Block b;
+				if (block != null)
+					b = block;
+				else
+					return true;
+				DoubleChestMatcher chestMatcher = new DoubleChestMatcher();
+				DoorMatcher doorMatcher = new DoorMatcher();
+				if (GunsPlus.lwc.getProtectionSet(b.getWorld(), b.getX(),
+						b.getY(), b.getZ()).size() >= 2) {
+					ProtectionFinder pf = new ProtectionFinder(GunsPlus.lwc);
+					pf.addBlock(b);
+					if (chestMatcher.matches(pf) || doorMatcher.matches(pf)) {
+						List<Block> list = pf.getBlocks();
+						for (Block bl : list) {
+							if (!GunsPlus.lwc.canAccessProtection(player, bl)) {
+								return false;
+							}
+						}
+					}
+				}
+				if (!GunsPlus.lwc.canAccessProtection(player, b)) {
+					return false;
+				} else
+					return true;
+			} else
+				return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 	
 	public void zoom(SpoutPlayer sp){
 		if(!objects.containsKey("ZOOMTEXTURE")){
