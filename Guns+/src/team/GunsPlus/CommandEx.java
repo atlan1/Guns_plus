8f6b5f78d7f5f7a2f72bfa575c79a32c7925a9e5
SirTyler <sirtyler132@gmail.com>
2012-04-14 19:44:22 -0400

    Began 1.2-DEV
    Changed CommandEx
    -Added List Command
    -Added Help Command
    -Fixed Reload Command
    
    Signed-off-by: SirTyler <sirtyler132@gmail.com>

--- Guns+/src/team/GunsPlus/CommandEx.java
+++ Guns+/src/team/GunsPlus/CommandEx.java
@@ -5,41 +5,45 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.getspout.spoutapi.material.CustomItem;
 
 public class CommandEx implements CommandExecutor{
 	private GunsPlus plugin;
 	
 	public CommandEx(GunsPlus gunsPlus) {
 		plugin = gunsPlus;
+		help[0] = (ChatColor.GOLD + "------Guns+ Version:" + plugin.getDescription().getVersion() + "-------");
+		help[1] = (ChatColor.BLUE + "-   /Guns+ Credits   ~  Displays Credits of Guns+");
+		help[2] = (ChatColor.BLUE + "-   /Guns+ Reload    ~  Reloads Config");
+		help[3] = (ChatColor.BLUE + "-   /Guns+ List      ~  Lists all Loaded Items");
 	}
+	String[] help = new String[4];
 
+	@SuppressWarnings("static-access")
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender instanceof Player) {
-			Player player = (Player) sender;
-			if(args.length == 1) {
-				String cmd = args[0];
-				if(cmd.equalsIgnoreCase("credits")) {
-					player.sendMessage(GunsPlusListener.credit);
-					return true;
-				}else if(cmd.equalsIgnoreCase("reload")&&player.hasPermission("gunsplus.reload")){
+		if(args.length == 1) {
+			String cmd = args[0];
+			if(cmd.equalsIgnoreCase("credits")) {
+				sender.sendMessage(GunsPlusListener.credit);
+				return true;
+			} else if(cmd.equalsIgnoreCase("reload")) {
+				if(sender instanceof Player && !((Player) sender).hasPermission("gunsplus.reload")) {
+					sender.sendMessage(ChatColor.RED + "Permission Denied");
+				} else {
 					plugin.config();
-					plugin.resetFields();
-					plugin.init();
-					sender.sendMessage(ChatColor.GREEN + GunsPlus.PRE + " Configuration files reloaded!");
+					sender.sendMessage(ChatColor.YELLOW + GunsPlus.PRE + " Configuration files reloaded!");
 				}
-			}
-		} else {
-			//Console
-			if(args.length == 1) {
-				String cmd = args[0];
-				if(cmd.equalsIgnoreCase("credits")) {
-					sender.sendMessage(GunsPlusListener.credit);
-					return true;
-				}else if(cmd.equalsIgnoreCase("reload")){
-					plugin.config();
-					sender.sendMessage(GunsPlus.PRE + " Configuration files reloaded!");
+				return true;
+			} else if(cmd.equalsIgnoreCase("help")) {
+				sender.sendMessage(help);
+				return true;
+			} else if(cmd.equalsIgnoreCase("list")) {
+				sender.sendMessage(ChatColor.BLUE + "---Guns----");
+				for(CustomItem c : plugin.allGuns) {
+					sender.sendMessage(ChatColor.GRAY + "- " + c.getName());
 				}
+				return true;
 			}
 		}
 		return false;
