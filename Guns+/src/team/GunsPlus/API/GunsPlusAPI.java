d4c99fd62cae60bd9eda07d7faa86b36dcc271f6
Robert Klassert <robert.klassert@arcor.de>
2012-04-16 19:56:56 +0200

    added useful methods for API users

--- Guns+/src/team/GunsPlus/API/GunsPlusAPI.java
+++ Guns+/src/team/GunsPlus/API/GunsPlusAPI.java
@@ -1,9 +1,21 @@
 package team.GunsPlus.API;
 
-import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.List;
 
-import team.GunsPlus.GunsPlus;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
 import team.GunsPlus.GunsPlusPlayer;
+import team.GunsPlus.Block.TripodData;
+import team.GunsPlus.Item.Addition;
+import team.GunsPlus.Item.Ammo;
+import team.GunsPlus.Item.Gun;
+import team.GunsPlus.Manager.GunsPlus;
+import team.GunsPlus.Util.GunUtils;
+import team.GunsPlus.Util.PlayerUtils;
 
 public class GunsPlusAPI {
 	private GunsPlus plugin;
@@ -18,8 +30,44 @@ public class GunsPlusAPI {
 	
 	public GunsPlusPlayer getPlayer(Player player) {
 		GunsPlusPlayer p = null;
-		p = GunsPlus.GunsPlusPlayers.get(GunsPlus.GunsPlusPlayers.indexOf(player));
+		p = PlayerUtils.getPlayerBySpoutPlayer((SpoutPlayer)p);
 		return p;
+	}
+	
+	public List<Gun> getAllGuns() {
+		return GunsPlus.allGuns;
+	}
+	
+	public Gun getGunByName(String name){
+		return GunUtils.getGun(name);
+	}
+	
+	public Gun getGunByItemStack(ItemStack itemstack){
+		return GunUtils.getGun(itemstack);
+	}
+	
+	public boolean isGun(ItemStack itemstack){
+		return GunUtils.isGun(itemstack);
+	}
+	
+	public List<Ammo> getAllAmmo(){
+		return GunsPlus.allAmmo;
+	}
+	
+	public List<Addition> getAllAdditions(){
+		return GunsPlus.allAdditions;
+	}
+	
+	public List<Block> getAllTripodBlocks(){
+		List<Block> blocklist = new ArrayList<Block>();
+		for(TripodData td : GunsPlus.allTripodBlocks){
+			blocklist.add(td.getLocation().getBlock());
+		}
+		return blocklist;
+	}
+	
+	public List<GunsPlusPlayer> getOnlineGunsPlusPlayers(){
+		return GunsPlus.GunsPlusPlayers;
 	}
 	
 }
