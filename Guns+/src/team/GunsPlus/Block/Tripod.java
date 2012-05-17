d42e4c27d18c7030f1503ff585d118d60dd6a31a
Robert Klassert <robert.klassert@arcor.de>
2012-05-17 18:21:20 +0200

    You have to place the tripod block on top of another.  Otherwise it will be removed.
    Signed-off-by: Robert Klassert <robert.klassert@arcor.de>

--- Guns+/src/team/GunsPlus/Block/Tripod.java
+++ Guns+/src/team/GunsPlus/Block/Tripod.java
@@ -3,6 +3,7 @@ package team.GunsPlus.Block;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.LivingEntity;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
 import org.getspout.spoutapi.material.MaterialData;
@@ -31,6 +32,10 @@ public class Tripod extends GenericCustomBlock {
 		Location l = new Location(w, x, y, z);
 		if (le instanceof SpoutPlayer) {
 			SpoutPlayer sp = (SpoutPlayer) le;
+			if(l.getBlock().getRelative(BlockFace.DOWN).getTypeId()==0||l.getBlock().getRelative(BlockFace.DOWN).isLiquid()){
+				remove(l);
+				return;
+			}
 			if (TripodDataHandler.getIdsByPlayers(sp.getName()).size() < GunsPlus.maxtripodcount
 					|| GunsPlus.maxtripodcount < 0) {
 				TripodData td = new TripodData(
@@ -43,15 +48,19 @@ public class Tripod extends GenericCustomBlock {
 							+ "You reached the maximum", ChatColor.RED
 							+ "amount of Tripods!", new SpoutItemStack(this),
 							2000);
-				//the block won't break if you call breakNaturally right now
-				Task removeBlock = new Task(GunsPlus.plugin, l){
-					public void run(){
-						Location l = (Location)this.getArg(0);
-						l.getBlock().breakNaturally();
-					}
-				};
-				removeBlock.startTaskDelayed(2);
+				remove(l);
 			}
 		}
+	}
+	
+	private void remove(Location l){
+		Task removeBlock = new Task(GunsPlus.plugin, l){
+			public void run(){
+				Location l = (Location)this.getArg(0);
+				l.getWorld().dropItemNaturally(l, new SpoutItemStack(GunsPlus.tripod));
+				l.getBlock().breakNaturally();
+			}
+		};
+		removeBlock.startTaskDelayed(1);
 	}
 }
