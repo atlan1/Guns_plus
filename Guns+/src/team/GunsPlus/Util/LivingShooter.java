38e90cd328e7f60155112b935a861c69fd5e6d6d
Robert Klassert <robert.klassert@arcor.de>
2012-05-17 18:33:24 +0200

    Added EffectUtils for  better effect handling.
    Signed-off-by: Robert Klassert <robert.klassert@arcor.de>

---
+++ Guns+/src/team/GunsPlus/Util/LivingShooter.java
@@ -0,0 +1,10 @@
+package team.GunsPlus.Util;
+
+import org.bukkit.entity.LivingEntity;
+
+
+public abstract class LivingShooter extends Shooter{
+
+	public abstract LivingEntity getLivingEntity();
+
+}
